45b75a2667b46202de32f948560b60fda1c879cb
ecappz3420 <info@ecappz.in>
2024-12-13 22:36:14 +0530

    Modified project directories

---
+++ api/fetch-token.js
@@ -0,0 +1,10 @@
+import { fetchAccessToken } from '../controller/controllerZohoData';
+
+export default async function handler(req, res) {
+  try {
+    const accessToken = await fetchAccessToken();
+    res.status(200).json({access_token: accessToken});
+  } catch (error) {
+    res.status(500).json({error: "Error fetching access token"});
+  }
+}
---
+++ api/get-records.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export default async function handler(req, res) {
+ try {
+    
+ } catch (error) {
+    
+ }
+}
